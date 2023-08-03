cae509777ede4dff13a5a5be17beb2032fb40a7d
unknown <rjanapareddy@vivifyhealthcare.com>
2023-08-03 23:27:37 +0530

    nnn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import axios from 'axios';
 import { TextField, Button, Typography, Box, Card, CardContent } from '@material-ui/core';
 
 const WeatherApp = () => {
@@ -15,11 +14,10 @@ const WeatherApp = () => {
     const apiKey = '0eb75fbb260fe905c23785f0c50f02ba';
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-    axios
-      .get(apiUrl)
-      .then((response) => {
-        console.log(response.data)
-        setWeatherData(response.data);
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setWeatherData(data);
         setShowWeatherCard(true);
       })
       .catch((error) => {
@@ -29,8 +27,11 @@ const WeatherApp = () => {
       });
   };
 
-  const backgroundImageUrl = 'url("https://images.pexels.com/photos/2114014/pexels-photo-2114014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")';
-  const cardBackGroundImgUrl = 'url("https://images.pexels.com/photos/2909083/pexels-photo-2909083.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")';
+  const backgroundImageUrl =
+    'url("https://images.pexels.com/photos/2114014/pexels-photo-2114014.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")';
+  const cardBackGroundImgUrl =
+    'url("https://images.pexels.com/photos/2909083/pexels-photo-2909083.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")';
+
   return (
     <Box
       m={2}
@@ -38,46 +39,56 @@ const WeatherApp = () => {
         backgroundImage: backgroundImageUrl,
         backgroundSize: 'cover',
         minHeight: '100vh',
-        display: 'flex', // Use flex display for horizontal alignment
-        alignItems: 'center', // Center vertically
-        justifyContent: 'space-around', // Space evenly
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-around',
       }}
     >
       <Box>
-        <Typography variant="h4" gutterBottom style={{ color: 'White',textAlign:"center"}}>
+        <Typography variant="h4" gutterBottom style={{ color: 'white', textAlign: 'center' }}>
           Weather by city
         </Typography>
-        <p style={{ color: 'White',textAlign:"center" }}>please enter the city name</p>
+        <p style={{ color: 'white', textAlign: 'center' }}>please enter the city name</p>
         <TextField
           label="Enter City Name"
           value={city}
           onChange={handleCityChange}
           variant="outlined"
         />
-        <div>
-        <Button variant="contained" color="primary" onClick={getWeatherData} style={{marginTop:"11px"}}>
+        <Button variant="contained" color="primary" onClick={getWeatherData} style={{ marginTop: '11px' }}>
           Get Weather
         </Button>
-        </div>
       </Box>
       {showWeatherCard && weatherData && (
         <Card
-        
           variant="outlined"
           style={{
-            // backgroundColor: 'rgba(255, 255, 255, 0.7)',
             backgroundImage: cardBackGroundImgUrl,
             width: 500,
             height: 400,
           }}
         >
           <CardContent>
-            <p style={{ color: 'White',textAlign:"center"}}>Clouds </p>
-            <Typography variant="h6"><span>Weather Details for city: </span>{weatherData.name}</Typography>
-            <Typography variant="body1"><span>Summary: </span>{weatherData.weather[0].description}</Typography>
-            <Typography variant="body1">{`Temperature: ${weatherData.main.temp}°C`}</Typography>
-            <Typography variant="body1">{`Humidity: ${weatherData.main.humidity}%`}</Typography>
-            <Typography variant="body1">{`Wind Speed: ${weatherData.wind.speed} m/s`}</Typography>
+            <p style={{ color: 'white', textAlign: 'center' }}>Clouds</p>
+            <Typography variant="h6">
+              <span>Weather Details for city: </span>
+              {weatherData.name}
+            </Typography>
+            {weatherData.weather && weatherData.weather.length > 0 && (
+              <Typography variant="body1">
+                <span>Summary: </span>
+                {weatherData.weather[0].description}
+              </Typography>
+            )}
+            {weatherData.main && (
+              <>
+                <Typography variant="body1">{`Temperature: ${weatherData.main.temp}°C`}</Typography>
+                <Typography variant="body1">{`Humidity: ${weatherData.main.humidity}%`}</Typography>
+              </>
+            )}
+            {weatherData.wind && (
+              <Typography variant="body1">{`Wind Speed: ${weatherData.wind.speed} m/s`}</Typography>
+            )}
           </CardContent>
         </Card>
       )}
