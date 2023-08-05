84ab5a6cc56c1098acf0085d63f335d93004bdfb
unknown <rjanapareddy@vivifyhealthcare.com>
2023-08-05 17:21:49 +0530

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render, screen, debug } from '@testing-library/react';
-import App from './App';
-
-test('renders weather by city', () => {
-  render(<App />);
-  
-});
