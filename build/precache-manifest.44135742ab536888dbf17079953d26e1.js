40cac6829c05b6cf915a87d530f5f685bee88ca1
unknown <rjanapareddy@vivifyhealthcare.com>
2023-08-05 18:35:19 +0530

    re delopyed and also re run the project

---
+++ build/precache-manifest.44135742ab536888dbf17079953d26e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e1e0eb0f840004c616f",
+    "url": "/Weather-App-React/static/css/main.4a947f67.chunk.css"
+  },
+  {
+    "revision": "0e1e0eb0f840004c616f",
+    "url": "/Weather-App-React/static/js/main.0e1e0eb0.chunk.js"
+  },
+  {
+    "revision": "efef7f6fa880e78d2a4c",
+    "url": "/Weather-App-React/static/js/1.efef7f6f.chunk.js"
+  },
+  {
+    "revision": "9c702aadd2a32cdd6e42",
+    "url": "/Weather-App-React/static/js/2.9c702aad.chunk.js"
+  },
+  {
+    "revision": "6c47188a5e541a677bd3",
+    "url": "/Weather-App-React/static/js/runtime~main.6c47188a.js"
+  },
+  {
+    "revision": "db5011362d9a2bdc64fcc92c001f7d02",
+    "url": "/Weather-App-React/index.html"
+  }
+];
